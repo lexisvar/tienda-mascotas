d61c3e66e6bf46f7f32475b39e68ec5de7af9179
lexisvar <lexisvar@gmail.com>
2021-12-11 11:21:59 -0500

    contar productos y mostrarlos en el template

--- mascotas/views.py
+++ mascotas/views.py
@@ -16,6 +16,15 @@ from django import forms
 class Dashboard(ListView): 
     model = Mascotas
 
+    def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      context['cantidad_mascotas'] = Mascotas.objects.count()
+      context['cantidad_clientes'] = Clientes.objects.count()
+      context['cantidad_productos'] = Productos.objects.count()
+      context['cantidad_servicios'] = Servicios.objects.count()
+      return context
+
+
 ############ MASCOTAS #############
 
 class MascotasListado(ListView): 
